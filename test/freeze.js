119c02e3b9961d25f02d53b353ea0ea8bc2417fc
smikes <smikes@cubane.com>
2015-01-10 20:54:21 -0700

    add freeze/thaw

---
+++ test/freeze.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var Purefts = require('..');
+
+var Code = require('code');
+var Lab = require('lab');
+var lab = Lab.script();
+var fs = require('fs');
+exports.lab = lab;
+
+var describe = lab.describe;
+var it = lab.it;
+var before = lab.before;
+var after = lab.after;
+var expect = Code.expect;
+
+describe('can freeze', function () {
+    it('can freeze an object to a file', function (done) {
+        var p = new Purefts(),
+            item = {name: 'foo', description: 'a foo thing'};
+
+        p.add(item);
+
+        p.freeze('./foo', function (err) {
+            expect(err).to.equal(null);
+
+            Purefts.thaw("./foo", function (err, q) {
+                expect(err).to.equal(null);
+                expect(q).to.be.instanceof(Purefts);
+                done();
+            });
+        });
+    });
+
+    it('handles failed callbacks', function (done) {
+        var p = new Purefts(),
+            cb = p.freeze.onBuffer('foo', function (err) {
+            expect(err).to.be.instanceof(Error);
+            done();
+        });
+
+        cb(new Error('bad zipfile'));
+    });
+});
---
+++ test/thaw.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var Purefts = require('..');
+
+var Code = require('code');
+var Lab = require('lab');
+var lab = Lab.script();
+var fs = require('fs');
+exports.lab = lab;
+
+var describe = lab.describe;
+var it = lab.it;
+var expect = Code.expect;
+
+describe('thaw errors', function () {
+    it('errors on invalid version', function (done) {
+        var thawer = Purefts.thaw.chooseThawer('bad');
+
+        thawer(null, function (err) {
+            expect(err).to.be.instanceof(Error);
+            expect(err.message).to.match(/unknown version "bad"/);
+            done();
+        });
+    });
+});
+
+describe('valid thaw', function () {
+    it('can thaw v1.0.0', function (done) {
+        Purefts.thaw('test/fixtures/v1-0-0-test', function (err, p) {
+            var expected = {"name": "foo", "description": "a foo thing"};
+
+            expect(err).to.equal(null);
+
+            expect(p.search('foo')).to.deep.equal(['foo']);
+            
+            expect(p.find({name: 'foo'})).to.deep.equal(expected);
+            
+            done();
+        });
+    });
+});
