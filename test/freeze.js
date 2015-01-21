41873701eee94e6e44e691c32567748ae9d1f669
smikes <smikes@cubane.com>
2015-01-21 13:26:02 -0700

    use bags, spread out values

--- test/freeze.js
+++ test/freeze.js
@@ -14,9 +14,10 @@ var it = lab.it;
 var expect = Code.expect;
 
 describe('can freeze', function () {
+    var item = {name: 'foo', description: 'a foo thing', fts: 'a foo thing'};
+
     it('can freeze an object to a file', function (done) {
-        var p = new Purefts(),
-            item = {name: 'foo', description: 'a foo thing'};
+        var p = new Purefts();
 
         p.add(item);
 
@@ -27,9 +28,257 @@ describe('can freeze', function () {
                 expect(err).to.equal(null);
                 expect(q).to.be.instanceof(Purefts);
 
-                rimraf("./foo", done);
+                q.get('foo', function (err, val) {
+                    expect(err).to.equal(null);
+                    expect(val).to.deep.equal(item);
+
+                    rimraf("./foo", done);
+                });
             });
         });
     });
 
+    it('can freeze an object to a file', function (done) {
+        var p = new Purefts();
+
+        p.add(item);
+
+        p.freeze('./foo', function (err) {
+            expect(err).to.equal(null);
+
+            Purefts.thaw("./foo", function (err, q) {
+                expect(err).to.equal(null);
+                expect(q).to.be.instanceof(Purefts);
+
+                q.get('bar', function (err, val) {
+                    expect(err).to.be.instanceof(Error);
+                    expect(val).to.equal();
+
+                    rimraf("./foo", done);
+                });
+            });
+        });
+    });
+});
+
+describe('a large test, >1024 objects', function () {
+    var item = {name: 'foo', description: 'a foo thing', fts: 'a foo thing'};
+
+    function zeroPad(i) {
+        var v = i.toString(10);
+        while (v.length < 4) {
+            v = '0' + v;
+        }
+        return v;
+    }
+
+    lab.before(function (done) {
+        var p = new Purefts(),
+            i,
+            obj;
+
+        for (i = 0; i < 2500; i += 1) {
+            obj = Object.create(item);
+            obj.name = 'foo' + zeroPad(i);
+            p.add(obj);
+        }
+
+        p.freeze('./foo', function (err) {
+            expect(err).to.equal(null);
+            done();
+        });
+    });
+
+    lab.after(function (done) {
+        rimraf('./foo', done);
+    });
+
+    it('can find first object in first bag', function (done) {
+
+        Purefts.thaw("./foo", function (err, q) {
+            expect(err).to.equal(null);
+            expect(q).to.be.instanceof(Purefts);
+
+            q.get('foo0001', function (err, val) {
+                expect(err).to.equal(null);
+                expect(val.name).to.equal('foo0001');
+
+                done();
+            });
+        });
+    });
+
+    it('can find an object twice', function (done) {
+
+        Purefts.thaw("./foo", function (err, q) {
+            expect(err).to.equal(null);
+            expect(q).to.be.instanceof(Purefts);
+
+            q.get('foo0001', function (err, val) {
+                expect(err).to.equal(null);
+                expect(val.name).to.equal('foo0001');
+
+                q.get('foo0410', function (err, val) {
+                    expect(err).to.equal(null);
+                    expect(val.name).to.equal('foo0410');
+
+                    done();
+                });
+            });
+        });
+    });
+
+    it('errors on missing object', function (done) {
+
+        Purefts.thaw("./foo", function (err, q) {
+            expect(err).to.equal(null);
+            expect(q).to.be.instanceof(Purefts);
+
+            q.get('foo0001.0', function (err, val) {
+                expect(err.message).to.match(/No object with name/);
+                expect(val).to.equal();
+
+                done();
+            });
+        });
+    });
+
+    it('can find last object in first bag', function (done) {
+
+        Purefts.thaw("./foo", function (err, q) {
+            expect(err).to.equal(null);
+            expect(q).to.be.instanceof(Purefts);
+
+            q.get('foo1023', function (err, val) {
+                expect(err).to.equal(null);
+                expect(val.name).to.equal('foo1023');
+
+                done();
+            });
+        });
+    });
+
+    it('can find first object in second bag', function (done) {
+
+        Purefts.thaw("./foo", function (err, q) {
+            expect(err).to.equal(null);
+            expect(q).to.be.instanceof(Purefts);
+
+            q.get('foo1024', function (err, val) {
+                expect(err).to.equal(null);
+                expect(val.name).to.equal('foo1024');
+
+                done();
+            });
+        });
+    });
+
+    it('can find first object in second bag', function (done) {
+
+        Purefts.thaw("./foo", function (err, q) {
+            expect(err).to.equal(null);
+            expect(q).to.be.instanceof(Purefts);
+
+            q.get('foo2047', function (err, val) {
+                expect(err).to.equal(null);
+                expect(val.name).to.equal('foo2047');
+
+                done();
+            });
+        });
+    });
+
+    it('can find first object in last bag', function (done) {
+
+        Purefts.thaw("./foo", function (err, q) {
+            expect(err).to.equal(null);
+            expect(q).to.be.instanceof(Purefts);
+
+            q.get('foo2048', function (err, val) {
+                expect(err).to.equal(null);
+                expect(val.name).to.equal('foo2048');
+
+                done();
+            });
+        });
+    });
+
+    it('can find last object in last bag', function (done) {
+
+        Purefts.thaw("./foo", function (err, q) {
+            expect(err).to.equal(null);
+            expect(q).to.be.instanceof(Purefts);
+
+            q.get('foo2499', function (err, val) {
+                expect(err).to.equal(null);
+                expect(val.name).to.equal('foo2499');
+
+                done();
+            });
+        });
+    });
+});
+
+
+describe('an exact multiple test, 1024 objects', function () {
+    var item = {name: 'foo', description: 'a foo thing', fts: 'a foo thing'};
+
+    function zeroPad(i) {
+        var v = i.toString(10);
+        while (v.length < 4) {
+            v = '0' + v;
+        }
+        return v;
+    }
+
+    lab.before(function (done) {
+        var p = new Purefts(),
+            i,
+            obj;
+
+        for (i = 0; i < 1024; i += 1) {
+            obj = Object.create(item);
+            obj.name = 'foo' + zeroPad(i);
+            p.add(obj);
+        }
+
+        p.freeze('./foo', function (err) {
+            expect(err).to.equal(null);
+            done();
+        });
+    });
+
+    lab.after(function (done) {
+        rimraf('./foo', done);
+    });
+
+    it('can find first object in only bag', function (done) {
+
+        Purefts.thaw("./foo", function (err, q) {
+            expect(err).to.equal(null);
+            expect(q).to.be.instanceof(Purefts);
+
+            q.get('foo0001', function (err, val) {
+                expect(err).to.equal(null);
+                expect(val.name).to.equal('foo0001');
+
+                done();
+            });
+        });
+    });
+
+    it('can find last object in only bag', function (done) {
+
+        Purefts.thaw("./foo", function (err, q) {
+            expect(err).to.equal(null);
+            expect(q).to.be.instanceof(Purefts);
+
+            q.get('foo1023', function (err, val) {
+                expect(err).to.equal(null);
+                expect(val.name).to.equal('foo1023');
+
+                done();
+            });
+        });
+    });
 });
